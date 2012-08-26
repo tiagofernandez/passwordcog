2706e3cee0300b34f86aef4f81a13f8340c8c039
Tiago Fernandez <tiago.fernandez@gmail.com>
2012-08-26 10:45:55 +0200

    Go back to initial state only when definitely entering background.

--- Passwordcog/PasswordcogAppDelegate.m
+++ Passwordcog/PasswordcogAppDelegate.m
@@ -50,6 +50,11 @@ static NSString *LocalStoreName = @"Passwordcog.sqlite";
 // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state. Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 - (void)applicationWillResignActive:(UIApplication *)application
 {
+}
+
+// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
   if ([PasswordcogAppDelegate userInterfaceIdiomPad]) {
     UINavigationController *rootVC = (UINavigationController *) self.window.rootViewController;
     UINavigationController *rootNavVC = [rootVC.viewControllers objectAtIndex:0];
@@ -62,11 +67,6 @@ static NSString *LocalStoreName = @"Passwordcog.sqlite";
     [rootVC dismissModalViewControllerAnimated:NO];
     [rootVC popToRootViewControllerAnimated:NO];
   }
-}
-
-// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
 }
 
 // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
